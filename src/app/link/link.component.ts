93f91eeb048717ba08ac975463f8d2ff785e68f5
rebeccaycheung <rebeccaycheung@gmail.com>
2019-12-18 19:16:55 +1100

    added font and link component

---
+++ src/app/link/link.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-link',
+  templateUrl: './link.component.html',
+  styleUrls: ['./link.component.css']
+})
+export class LinkComponent implements OnInit {
+  @Input('heading') headingName: string;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
