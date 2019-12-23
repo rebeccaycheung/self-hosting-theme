03b7376cb7b216628cb5a2e2e9dd66f86e651dc4
rebeccaycheung <rebeccaycheung@gmail.com>
2019-12-23 20:58:04 +1100

    added categories for links

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
-import { faPen, faTv, faPiggyBank, faCloud } from '@fortawesome/free-solid-svg-icons';
-import { faReddit, faQq } from '@fortawesome/fontawesome-free-brands';
+import { faPen, faTv, faPiggyBank, faCloud, faDollarSign, faRunning, faBook, faTrafficLight, faImage } from '@fortawesome/free-solid-svg-icons';
+import { faReddit, faQq, faTwitch, faYoutube } from '@fortawesome/fontawesome-free-brands';
 
 @Component({
   selector: 'app-root',
@@ -8,28 +8,36 @@ import { faReddit, faQq } from '@fortawesome/fontawesome-free-brands';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'title';
+  finance = 'finance';
+  media = 'media';
+  admin = 'admin';
 
-  links = [
-    {name: 'Notion', icon: faPen},
-    {name: 'Plex', icon: faTv},
-    {name: 'Firefly', icon: faPiggyBank},
+  finances = [
+    {name: 'BOQ', icon: faDollarSign},
+    {name: 'NAB', icon: faDollarSign},
+    {name: 'Firefly', icon: faPiggyBank}
+  ];
+
+  medias = [
     {name: 'Reddit', icon: faReddit},
+    {name: 'Plex', icon: faTv},
+    {name: 'Jellyfin', icon: faTv},
+    {name: 'Youtube', icon: faYoutube},
+    {name: 'twitch', icon: faTwitch},
+    {name: 'Kayo', icon: faRunning},
+    {name: 'Piwigo', icon: faImage}
+  ];
+
+  admins = [
+    {name: 'Notion', icon: faPen},
     {name: 'NextCloud', icon: faCloud},
     {name: 'qBittorrent', icon: faQq},
-    {name: 'BOQ', icon: faPiggyBank},
-    {name: 'NAB', icon: faPiggyBank},
     {name: 'Namecheap', icon: faPiggyBank},
     {name: 'Glances', icon: faPiggyBank},
     {name: 'Funkwhale', icon: faPiggyBank},
-    {name: 'Bookstack', icon: faPiggyBank},
+    {name: 'Bookstack', icon: faBook},
     {name: 'pihole', icon: faPiggyBank},
-    {name: 'Piwigo', icon: faPiggyBank},
-    {name: 'Jellyfin', icon: faPiggyBank},
-    {name: 'Traefik', icon: faPiggyBank},
-    {name: 'Youtube', icon: faPiggyBank},
-    {name: 'twitch', icon: faPiggyBank},
-    {name: 'Kayo', icon: faPiggyBank}
+    {name: 'Traefik', icon: faTrafficLight}
   ];
 
 }
